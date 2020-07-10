319de6baf1a7e7f82da666ac84bc496c9a776409
Victor Scholz <vgscholz@gmail.com>
2020-07-10 02:14:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,7 +24,7 @@ def get_japanese_emoticon(emote, emoticons)
   end
   # binding.pry
   emotion ? library[emoticon][:japanese] : "Sorry, that emoticon was not found"
-  binding.pry
+  # binding.pry
 end
 
 def get_english_meaning(emote, emoticons)
